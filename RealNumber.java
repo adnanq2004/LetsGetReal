2e9afdc97291ebff4c8c0e6cfa4a5906ef663e9f
adnanq2004 <aquayyum20@stuy.edu>
2020-11-22 16:00:49 -0500

    added basic info and first two functions

---
+++ RealNumber.java
@@ -0,0 +1,33 @@
+public class RealNumber {
+
+	private double value;
+
+	public RealNumber(double v) {
+		value = v;
+	}
+
+	public double getValue() {
+		return value;
+	}
+
+	public String toString() {
+		return "" + (this.getValue());
+	}
+
+	public boolean equals(RealNumber other) {
+		boolean val = false;
+		if (this.value == 0) {
+			val = (other.value == 0);
+		}
+		else {
+			val = Math.abs((this.value - other.value)/this.value) <= .00001; 
+		}
+		return val;
+	}
+
+	public RealNumber add(RealNumber other) {
+		RealNumber val = new RealNumber(value + other.value);
+		return val;
+	}
+
+}
