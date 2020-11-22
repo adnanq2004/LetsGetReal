132f6c713a387096a5a9e1d9aa77866fc79fb118
adnanq2004 <aquayyum20@stuy.edu>
2020-11-22 16:19:44 -0500

    added up to reciprocal function

---
+++ RationalNumber.java
@@ -0,0 +1,37 @@
+public class RationalNumber extends RealNumber {
+
+	private int numerator;
+	private int denominator;
+
+	public RationalNumber(int nume, int deno) {
+		super(0.0);
+		if (deno == 0) {
+			numerator = 0;
+			denominator = 1;
+		}
+		else {
+			numerator = nume;
+			denominator = deno;
+		}
+	}
+
+	public double getValue() {
+		return 0.0;
+	}
+
+	public int getNumerator() {
+		return numerator;
+	}
+
+	public int getDenominator() {
+		return denominator;
+	}
+
+	public RationalNumber reciprocal() {
+		RationalNumber val = new RationalNumber(denominator, numerator);
+		return val;
+	}
+
+
+
+}
