5ae0a8001b355d200d4fb218c39710c2fa31fd48
adnanq2004 <aquayyum20@stuy.edu>
2020-11-24 20:38:55 -0500

    i made the abstract number class with all of its methods

---
+++ Number.java
@@ -0,0 +1,30 @@
+public abstract class Number {
+
+	private int value;
+
+	public int getValue() {
+		return value;
+	}
+
+	public int compareTo(Number other) {
+		return value - other.value;
+		/*if (equals(other)) {
+			return 0;
+		}
+		else {
+			if ;
+		}*/
+	}
+
+	public boolean equals(Number other) {
+		if (getValue() == 0 || other.getValue() == 0) {
+			return (getValue() == 0 && other.getValue() == 0);
+		}
+		else {
+			int temp = getValue() - other.getValue();
+			temp = temp/getValue();
+			return (temp <= .00001);
+		}
+	}
+
+}
